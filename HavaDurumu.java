685b04ef08f7322487ed7f6a507db5ed5b3fc722
Hanne Zeynep Durhat <83821699+hzeynep55@users.noreply.github.com>
2022-03-11 10:51:42 +0300

    Update HavaDurumu.java

--- HavaDurumu.java
+++ HavaDurumu.java
@@ -5,12 +5,18 @@ public class HavaDurumu {
         Scanner inp=new Scanner(System.in);
         int sicaklik;
         System.out.print("Hava sıcaklığını yazınız: ");
-        sicaklik=inp.nextInt();
-        String  etkinlik=(sicaklik<5)?"Kayak yapmanı öneriyoruz :)":(sicaklik>=5 && sicaklik<15)
-                ?"Sinemaya gitmek için harika bir hava :)":(sicaklik>=15&&sicaklik<=25)
-                ?"Bir pikniğe gitmenin vakti gelmedi mi sence?":"Yüzme en iyi spor aktivesidir!";
+        try {
+            sicaklik = inp.nextInt();
+            String  etkinlik=(sicaklik<5)?"Kayak yapmanı öneriyoruz :)":(sicaklik>=5 && sicaklik<15)
+                    ?"Sinemaya gitmek için harika bir hava :)":(sicaklik>=15&&sicaklik<=25)
+                    ?"Bir pikniğe gitmenin vakti gelmedi mi sence?":"Yüzme en iyi spor aktivesidir!";
 
-        System.out.print(etkinlik);
+            System.out.print(etkinlik);
+        }
+        catch (Exception e){
+            System.out.print("Lütfen geçerli sıcaklık değerini giriniz.");
+        }
+
 
 
     }
